f8bbf8480e4d31d219d43c03957f41c1eacb0a05
WGOTTSCH <wolfgang@the-gottschalks.de>
2021-03-01 14:34:36 +0100

    same turtle example

---
+++ turtle/star.py
@@ -0,0 +1,11 @@
+# Python program to draw star
+# using Turtle Programming
+import turtle 
+ 
+star = turtle.Turtle()
+ 
+for i in range(40):
+    star.forward(150)
+    star.right(146)
+     
+turtle.done()
