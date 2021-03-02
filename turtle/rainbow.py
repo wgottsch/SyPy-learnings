c24399145da6ee7bb3fe6485bdad83c2199a834d
WGOTTSCH <wolfgang@the-gottschalks.de>
2021-03-02 18:48:20 +0100

    test

--- turtle/rainbow.py
+++ turtle/rainbow.py
@@ -2,11 +2,12 @@
 # Rainbow Benzene
 # using Turtle Programming
 import turtle
-colors = ['red', 'purple', 'blue', 'green', 'orange', 'yellow']
+#colors = ['red', 'purple', 'blue', 'green', 'orange', 'yellow']
+colors = ['blue', 'yellow']
 t = turtle.Pen()
 turtle.bgcolor('black')
 for x in range(360):
-    t.pencolor(colors[x%6])
+    t.pencolor(colors[x%2])
     t.width(x/100 + 1)
     t.forward(x)
     t.left(59)
